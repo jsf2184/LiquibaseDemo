749cdc70b088ea597bad2ebb7db169b8942f8b75
jsf2184 <jsf2184@gmail.com>
2021-03-29 13:13:54 -0500

    First commit

---
+++ src/test/java/com/jsf2184/liquify/LiquibaseDemo/LiquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsf2184.liquify.LiquibaseDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
